860e8572b0e2087c32038d8a0794b2e4313ec69a
Marcin Oszwałdowski <s22678@pjwstk.edu.pl>
2023-03-05 14:27:07 +0100

    added files

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text.RegularExpressions;
+
+Console.WriteLine("Hello, World!");
+// zle
+try {
+    var url = args[0];
+}
+catch (ArgumentNullException exception)
+{
+    Console.WriteLine("");
+}
+
+var regex = new Regex(@"[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+");
+var httpClient = new HttpClient();
+var httpResult = await httpClient.GetAsync(url);
+var httpContent = await httpResult.Content.ReadAsStringAsync();
+var matches = regex.Matches(httpContent);
+matches.Select(x => x.Value).Distinct().ToList().ForEach(x => Console.WriteLine(x));
